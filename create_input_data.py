226f4c2f7ef5dfd2c9debb2461dd7f9ba379a797
Joseph Higgins <josephhiggins@DN0a22b121.SUNet>
2017-11-13 12:52:08 -0800

    babys first svm

--- create_input_data.py
+++ create_input_data.py
@@ -23,14 +23,8 @@ df = df[~pd.isnull(df['radiant_picks'])]
 cutoff_date = '2017-05-01'
 df = df[df['start_date'] <= cutoff_date]
 
-#create train/dev set
-train_pct = 0.90
-msk = np.random.rand(len(df)) < train_pct
-df_train = df[msk]
-df_dev = df[~msk]
-
-#get hero pick indicators
-radiant_picks = df_train['radiant_picks']
+#get hero pick indicators for training data
+radiant_picks = df['radiant_picks']
 radiant_picks = list(map(lambda x: 
         list(map(lambda y: str(y), x))
     , radiant_picks))
@@ -39,7 +33,7 @@ radiant_picks = pd.Series(radiant_picks)
 radiant_picks = pd.DataFrame(radiant_picks)
 radiant_picks = radiant_picks[0].str.join(sep='*').str.get_dummies(sep='*')
 
-dire_picks = df_train['dire_picks']
+dire_picks = df['dire_picks']
 dire_picks = list(map(lambda x: 
         list(map(lambda y: str(y), x))
     , dire_picks))
@@ -53,15 +47,42 @@ dire_picks = dire_picks.rename(columns = lambda x: x+'_dire')
 
 pick_indicators = pd.concat([radiant_picks, dire_picks], axis = 1)
 
-#append victory indicators
-pick_indicators['match_id'] = list(df_train['match_id'])
-pick_indicators['radiant_win'] = list(df_train['radiant_win'])
+pick_indicators['match_id'] = list(df['match_id'])
+pick_indicators['radiant_win'] = list(df['radiant_win'])
 
+#create train/dev set
+train_pct = 0.90
+msk = np.random.rand(len(df)) < train_pct
+df_train = pick_indicators[msk]
+df_dev = pick_indicators[~msk]
 
+X_train = df_train.drop({'radiant_win', 'match_id'}, axis = 1)
+y_train = df_train['radiant_win']
 
+X_dev = df_dev.drop({'radiant_win', 'match_id'}, axis = 1)
+y_dev = df_dev['radiant_win']
+
+#train an SVM
+clf = svm.LinearSVC()
+clf.fit(X_train, y_train)
+predictions = clf.predict(X_dev)
+
+output = pd.DataFrame({
+         'prediction': list(predictions)
+        ,'actual': list(y_dev)
+})
+
+output['correct'] = output['prediction'] == output['actual']
+correct_predictions = np.sum(output['correct'] == True)
+dev_size = np.shape(output)[0]
+
+pct_correct = correct_predictions/dev_size
+pct_correct
+
+    
 '''
 pick_indicators['25_radiant'][pick_indicators['match_id'] == 3539847416]
-df.ix[3539847416]
+df.ix[3135929855]
 '''
 
 
