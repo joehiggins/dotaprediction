fc01357acf89c7c550cc1d3a78f68703ee4ee987
Joseph Higgins <josephhiggins@DN0a22b121.SUNet>
2017-11-13 12:10:06 -0800

    create input data

--- create_input_data.py
+++ create_input_data.py
@@ -9,14 +9,28 @@ Created on Sun Nov 12 20:43:47 2017
 import os
 import pandas as pd
 import numpy as np
+import math
+from sklearn import svm
 
-file_path = '/Users/josephhiggins/Documents/CS 229/Project/Tabular Data/'
+file_path = '/Users/josephhiggins/Documents/CS 229/Project/dotaprediction/Tabular Data/'
 file_name = 'dota2_pro_match_tabular_data.pkl'
 df = pd.read_pickle(file_path + file_name)
 
+#filter out nulls
+df = df[~pd.isnull(df['radiant_picks'])]
+
+#filter to before cutoff date
+cutoff_date = '2017-05-01'
+df = df[df['start_date'] <= cutoff_date]
+
+#create train/dev set
+train_pct = 0.90
+msk = np.random.rand(len(df)) < train_pct
+df_train = df[msk]
+df_dev = df[~msk]
 
 #get hero pick indicators
-radiant_picks = df['radiant_picks'][~pd.isnull(df['radiant_picks'])]
+radiant_picks = df_train['radiant_picks']
 radiant_picks = list(map(lambda x: 
         list(map(lambda y: str(y), x))
     , radiant_picks))
@@ -25,7 +39,7 @@ radiant_picks = pd.Series(radiant_picks)
 radiant_picks = pd.DataFrame(radiant_picks)
 radiant_picks = radiant_picks[0].str.join(sep='*').str.get_dummies(sep='*')
 
-dire_picks = df['dire_picks'][~pd.isnull(df['radiant_picks'])]
+dire_picks = df_train['dire_picks']
 dire_picks = list(map(lambda x: 
         list(map(lambda y: str(y), x))
     , dire_picks))
@@ -39,23 +53,18 @@ dire_picks = dire_picks.rename(columns = lambda x: x+'_dire')
 
 pick_indicators = pd.concat([radiant_picks, dire_picks], axis = 1)
 
-
-
-
+#append victory indicators
+pick_indicators['match_id'] = list(df_train['match_id'])
+pick_indicators['radiant_win'] = list(df_train['radiant_win'])
 
 
 
 '''
-#get all unique player account_ids
-a = pd.Series(list(
-     zip(list(map(str, df['player_0_account_id'])),
-         list(map(str, df['player_1_account_id'])),
-         list(map(str, df['player_2_account_id'])),
-         list(map(str, df['player_3_account_id'])),
-         list(map(str, df['player_4_account_id'])),
-    )
-))
-a = pd.DataFrame(a)
-b = a.head(5)
-b = b[0].str.join(sep='*').str.get_dummies(sep='*')
+pick_indicators['25_radiant'][pick_indicators['match_id'] == 3539847416]
+df.ix[3539847416]
 '''
+
+
+
+
+
