17c661266841d3504b3c5e9d4a38f3add64bc69a
Joseph Higgins <josephhiggins@DN51s58o.SUNet>
2017-11-12 23:05:34 -0800

    hero indicator vars

--- create_input_data.py
+++ create_input_data.py
@@ -15,22 +15,37 @@ file_name = 'dota2_pro_match_tabular_data.pkl'
 df = pd.read_pickle(file_path + file_name)
 
 
-account_ids = [
-        list(np.unique(df['player_0_account_id'])),
-        list(np.unique(df['player_1_account_id'])),
-        list(np.unique(df['player_2_account_id'])),
-        list(np.unique(df['player_3_account_id'])),
-        list(np.unique(df['player_4_account_id'])),
-        list(np.unique(df['player_5_account_id'])),
-        list(np.unique(df['player_6_account_id'])),
-        list(np.unique(df['player_7_account_id'])),
-        list(np.unique(df['player_8_account_id'])),
-        list(np.unique(df['player_9_account_id'])),
-]
-account_ids = [item for sublist in account_ids for item in sublist]
-account_ids = np.unique(account_ids)
+#get hero pick indicators
+radiant_picks = df['radiant_picks'][~pd.isnull(df['radiant_picks'])]
+radiant_picks = list(map(lambda x: 
+        list(map(lambda y: str(y), x))
+    , radiant_picks))
+radiant_picks = list(map(tuple, radiant_picks))
+radiant_picks = pd.Series(radiant_picks)
+radiant_picks = pd.DataFrame(radiant_picks)
+radiant_picks = radiant_picks[0].str.join(sep='*').str.get_dummies(sep='*')
+
+dire_picks = df['dire_picks'][~pd.isnull(df['radiant_picks'])]
+dire_picks = list(map(lambda x: 
+        list(map(lambda y: str(y), x))
+    , dire_picks))
+dire_picks = list(map(tuple, dire_picks))
+dire_picks = pd.Series(dire_picks)
+dire_picks = pd.DataFrame(dire_picks)
+dire_picks = dire_picks[0].str.join(sep='*').str.get_dummies(sep='*')
+
+radiant_picks = radiant_picks.rename(columns = lambda x: x+'_radiant')
+dire_picks = dire_picks.rename(columns = lambda x: x+'_dire')
+
+pick_indicators = pd.concat([radiant_picks, dire_picks], axis = 1)
 
 
+
+
+
+
+
+'''
 #get all unique player account_ids
 a = pd.Series(list(
      zip(list(map(str, df['player_0_account_id'])),
@@ -43,10 +58,4 @@ a = pd.Series(list(
 a = pd.DataFrame(a)
 b = a.head(5)
 b = b[0].str.join(sep='*').str.get_dummies(sep='*')
-
-
-
-pd.get_dummies(a)
-
-players = pd.Series(list('abca'))
-pd.get_dummies(s)
+'''
