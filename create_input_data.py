731a83dd2d97bc764ef0fa055a87538895f27228
Petra Grutzik <pgrutzik@stanford.edu>
2017-12-03 17:27:03 -0800

    working on cross validation for nn. might be better on SVM. still need to run

--- create_input_data.py
+++ create_input_data.py
@@ -12,6 +12,8 @@ import numpy as np
 import math
 from sklearn import svm
 
+CROSS_TRAIN = True
+
 file_path = '/Users/josephhiggins/Documents/CS 229/Project/dotaprediction/Tabular Data/'
 file_name = 'dota2_pro_match_tabular_data.pkl'
 df = pd.read_pickle(file_path + file_name)
@@ -96,24 +98,31 @@ team_indicators = pd.concat([radiant_team, dire_team], axis = 1)
 team_indicators = team_indicators.set_index(np.arange(0,np.shape(team_indicators)[0]))
 
 #create train/dev set
+
 all_features = pd.concat([pick_indicators, team_indicators], axis = 1)
 #all_features = pd.concat([team_indicators], axis = 1)
 all_features['match_id'] = list(df['match_id'])
 all_features['radiant_win'] = list(df['radiant_win'])
 
-train_pct = 0.90
-msk = np.random.rand(len(df)) < train_pct
-df_train = all_features[msk]
-df_dev = all_features[~msk]
-
 #Save output
 file_path = '/Users/josephhiggins/Documents/CS 229/Project/Input Data/'
 
-file_name = 'dota2_pro_match_input_data_train.pkl'
-df_train.to_pickle(file_path + file_name)
+if CROSS_TRAIN == False:
+    train_pct = 0.90
+    msk = np.random.rand(len(df)) < train_pct
+    df_train = all_features[msk]
+    df_dev = all_features[~msk]
 
-file_name = 'dota2_pro_match_input_data_dev.pkl'
-df_dev.to_pickle(file_path + file_name)
+    file_name = 'dota2_pro_match_input_data_train.pkl'
+    df_train.to_pickle(file_path + file_name)
+
+    file_name = 'dota2_pro_match_input_data_dev.pkl'
+    df_dev.to_pickle(file_path + file_name)
+else:
+    #for cross train
+    df.to_pickle(file_path + file_name)
+    file_name = 'dota2_pro_match_input_data_all.pkl
+
 
     
 '''
--- neuralnet.py
+++ neuralnet.py
@@ -6,21 +6,28 @@ import numpy as np
 import math
 import keras
 
-#Read in data
-file_path = '/Users/josephhiggins/Documents/CS 229/Project/Input Data/'
-file_name = 'dota2_pro_match_input_data_train.pkl'
-df_train = pd.read_pickle(file_path + file_name)
+CROSS_TRAIN = False
+NUM_CHUNKS = 10
+#Read in data 
+file_path = '/Users/petragrutzik/CSClasses/CS229/dota/dotaprediction/Input Data/' #diff btw input data and pro match details
 
-file_name = 'dota2_pro_match_input_data_dev.pkl'
-df_dev = pd.read_pickle(file_path + file_name)
+# #create train/dev set without cross train
+# file_name = 'dota2_pro_match_input_data_train.pkl'
+# df_train = pd.read_pickle(file_path + file_name)
 
-#create train/dev set
-X_train = df_train.drop({'radiant_win', 'match_id'}, axis = 1)
-y_train = df_train['radiant_win']
+# file_name = 'dota2_pro_match_input_data_dev.pkl'
+# df_dev = pd.read_pickle(file_path + file_name)
 
-X_dev = df_dev.drop({'radiant_win', 'match_id'}, axis = 1)
-y_dev = df_dev['radiant_win']
+# X_train = df_train.drop({'radiant_win', 'match_id'}, axis = 1)
+# y_train = df_train['radiant_win']
 
+# X_dev = df_dev.drop({'radiant_win', 'match_id'}, axis = 1)
+# y_dev = df_dev['radiant_win']
+
+#create train dev set for cross train
+file_name = 'dota2_pro_match_input_data_all.pkl'
+df_all = pd.read_pickle(file_path + file_name)
+length_chunk = math.floor(len(df_all)/NUM_CHUNKS)
 
 ########################
 #train a neural net
@@ -28,25 +35,44 @@ from keras.models import Sequential
 from keras.layers import Dense, Activation
 
 NUM_OUTPUT_UNITS = 1
-NUM_VARIABLES = np.shape(X_train)[1]
+NUM_VARIABLES = np.shape(df_all)[1] - 2 # because to train, we will later drop 2 columns:'radiant_win', 'match_id'
 
 model = Sequential()
 model.add(Dense(units=NUM_OUTPUT_UNITS, input_dim=NUM_VARIABLES, activation='relu')) 
 model.add(Dense(math.floor(NUM_VARIABLES/2), activation='relu'))
-model.add(Dense(8, activation='relu'))
 model.add(Dense(1, activation='sigmoid'))
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-# Fit the model
-model.fit(X_train.values, y_train, epochs=50, batch_size=10)
+if CROSS_TRAIN == True:
+    num_rounds_cross_train = NUM_CHUNKS
+else:
+    num_rounds_cross_train = 1
+for i in range(num_rounds_cross_train):
+    df_dev = df_all[i:i + length_chunk] 
+    df_train = df_all[i + length_chunk+1:] 
+    X_train = df_train.drop({'radiant_win', 'match_id'}, axis = 1)
+    y_train = df_train['radiant_win']
 
-# Test
-train_error = model.evaluate(X_train.values, y_train)
-dev_error = model.evaluate(X_dev.values, y_dev)
-print(dev_error)
+    X_dev = df_dev.drop({'radiant_win', 'match_id'}, axis = 1)
+    print(X_train)
+    y_dev = df_dev['radiant_win']
 
-dev_predictions = model.predict(X_dev.values)
-dev_predictions = [round(x[0]) for x in dev_predictions]
-model.metrics_names
+    # Fit the model
+    model.fit(X_train.values, y_train, epochs=50, batch_size=10)
+
+    # Test
+    train_error = model.evaluate(X_train.values, y_train)
+    dev_error = model.evaluate(X_dev.values, y_dev)
+    print(dev_error)
+
+    dev_predictions = model.predict(X_dev.values)
+    dev_predictions = [round(x[0]) for x in dev_predictions]
+    print(dev_predictions)
+    model.metrics_names
+
+
+# Pick the model Mi with the lowest estimated generalization error, and
+# retrain that model on the entire training set S. The resulting hypothesis
+# is then output as our final answer.
 
 ##########################
